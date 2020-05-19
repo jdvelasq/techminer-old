79122ea923741b8fc09fda0de22e1d6d58add99a
jdvelasq <jdvelasq@unal.edu.co>
2020-05-19 17:18:31 -0500

    self

--- techminer/dataframe.py
+++ techminer/dataframe.py
@@ -2850,5 +2850,7 @@ class DataFrame(pd.DataFrame):
         items = Keywords(x=top, ignore_case=False)
         colname = "top_{:d}_{:s}_freq".format(top_n, column.replace(" ", "_"))
         df = DataFrame(self.copy())
-        df[colname] = df[column].map(lambda x: any([e in items for e in x.split(sep)]))
+        df[colname] = self[column].map(
+            lambda x: any([e in items for e in x.split(sep)])
+        )
         return DataFrame(df)
