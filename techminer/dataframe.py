3a000810923f25bd37ad4d67c96b9988b54836f2
jdvelasq <jdvelasq@unal.edu.co>
2020-05-19 17:48:23 -0500

    most cited by function

--- techminer/dataframe.py
+++ techminer/dataframe.py
@@ -2874,13 +2874,68 @@ class DataFrame(pd.DataFrame):
         4     B,D             c;d         4   4                True
         5     A,B               d         5   5                True
 
-
         """
         top = self.documents_by_term(column, sep=sep)[column].head(top_n)
-        items = Keywords(x=top, ignore_case=False)
+        items = Keywords(x=top)
         colname = "top_{:d}_{:s}_freq".format(top_n, column.replace(" ", "_"))
         df = DataFrame(self.copy())
-        df[colname] = self[column].map(
-            lambda x: any([e in items for e in x.split(sep)])
-        )
+        if sep is None and column in SCOPUS_SEPS.keys():
+            sep = SCOPUS_SEPS[column]
+        if sep is not None:
+            df[colname] = self[column].map(
+                lambda x: any([e in items for e in x.split(sep)])
+            )
+        else:
+            df[colname] = self[column].map(lambda x: x in items)
+        return DataFrame(df)
+
+    def most_cited_by(self, column, top_n=10, sep=None):
+        """Creates a group for most items cited by 
+
+        Examples
+        ----------------------------------------------------------------------------------------------
+
+        >>> import pandas as pd
+        >>> x = [ 'A', 'A,B', 'B', 'A,B,C', 'B,D', 'A,B']
+        >>> y = [ 'a', 'a;b', 'b', 'c', 'c;d', 'd']
+        >>> df = pd.DataFrame(
+        ...    {
+        ...       'Authors': x,
+        ...       'Author Keywords': y,
+        ...       'Cited by': list(range(len(x))),
+        ...       'ID': list(range(len(x))),
+        ...    }
+        ... )
+        >>> df
+          Authors Author Keywords  Cited by  ID
+        0       A               a         0   0
+        1     A,B             a;b         1   1
+        2       B               b         2   2
+        3   A,B,C               c         3   3
+        4     B,D             c;d         4   4
+        5     A,B               d         5   5
+
+        >>> DataFrame(df).most_cited_by('Authors', top_n=1)
+          Authors Author Keywords  Cited by  ID  top_1_Authors_cited_by
+        0       A               a         0   0                   False
+        1     A,B             a;b         1   1                    True
+        2       B               b         2   2                    True
+        3   A,B,C               c         3   3                    True
+        4     B,D             c;d         4   4                    True
+        5     A,B               d         5   5                    True
+
+
+        """
+        top = self.citations_by_term(column, sep=sep)[column].head(top_n)
+        items = Keywords(x=top)
+        colname = "top_{:d}_{:s}_cited_by".format(top_n, column.replace(" ", "_"))
+        df = DataFrame(self.copy())
+        if sep is None and column in SCOPUS_SEPS.keys():
+            sep = SCOPUS_SEPS[column]
+        if sep is not None:
+            df[colname] = self[column].map(
+                lambda x: any([e in items for e in x.split(sep)])
+            )
+        else:
+            df[colname] = self[column].map(lambda x: x in items)
         return DataFrame(df)
