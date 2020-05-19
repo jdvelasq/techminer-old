c8ea5770a9b1b608cd0e9355652a9da9e9ee3b84
jdvelasq <jdvelasq@unal.edu.co>
2020-05-19 18:59:28 -0500

    ...

--- techminer/plot.py
+++ techminer/plot.py
@@ -19,7 +19,7 @@ from .chord_diagram import ChordDiagram
 def cut_text(w):
     if isinstance(w, (int, float)):
         return w
-    return w if len(w) < 35 else w[:31] + "... " + w[w.find("[") :]
+    return w if len(w) < 35 else w[:31] + " ..."
 
 
 class Plot:
