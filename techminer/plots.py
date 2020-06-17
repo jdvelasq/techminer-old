4385742178c42b878a6745453c51b8c604af1b31
jdvelasq <jdvelasq@unal.edu.co>
2020-06-17 04:28:28 +0000

    draw bordes in treemap

--- techminer/plots.py
+++ techminer/plots.py
@@ -1844,11 +1844,23 @@ def tree(x, cmap="Blues", figsize=(8, 8), alpha=0.9):
 
     x = x.copy()
     column0 = x[x.columns[0]]
+    column0 = [textwrap.shorten(text=text, width=TEXTLEN) for text in column0]
+    column0 = ["\n".join(text.split()) for text in column0]
     column1 = x[x.columns[1]]
     colors = [
-        cmap((0.2 + 0.75 * column1[i] / max(column1))) for i in range(len(column1))
+        cmap(0.4 + 0.60 * (column1[i] - min(column1))/ (max(column1) - min(column1))) for i in range(len(column1))
     ]
-    squarify.plot(sizes=column1, label=column0, color=colors, alpha=alpha, ax=ax)
+
+    squarify.plot(
+        sizes=column1,
+        label=column0,
+        color=colors,
+        alpha=alpha,
+        ax=ax,
+        pad=True,
+        bar_kwargs={"edgecolor": "k", "linewidth": 0.5},
+        text_kwargs={"color": "w", "fontsize": 9},
+    )
     ax.axis("off")
     return fig
 
