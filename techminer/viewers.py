1f403a04aec95b78b8f51851fece520802bec599
jdvelasq <jdvelasq@unal.edu.co>
2020-06-25 19:01:05 +0000

    new matrix explorer gui

--- techminer/viewers.py
+++ techminer/viewers.py
@@ -15,8 +15,6 @@ from techminer.explode import __explode
 
 from techminer.explode import MULTIVALUED_COLS
 
-WIDGET_WIDTH = "200px"
-
 
 def __record_to_HTML(x):
     """
@@ -111,14 +109,14 @@ def column(df, top_n=50):
             "desc": "Column:",
             "widget": widgets.Dropdown(
                 options=[z for z in COLUMNS if z in df.columns],
-                layout=Layout(width=WIDGET_WIDTH),
+                layout=Layout(width="90%"),
             ),
         },
         # 1
         {
             "arg": "value",
             "desc": "Term:",
-            "widget": widgets.Dropdown(options=[], layout=Layout(width=WIDGET_WIDTH),),
+            "widget": widgets.Dropdown(options=[], layout=Layout(width="90%"),),
         },
         # 2
         {
@@ -232,21 +230,21 @@ def matrix(df, top_n=50):
     # UI
     #
     # -------------------------------------------------------------------------
-    controls = [
+    left_panel = [
         # 0
         {
             "arg": "main_column",
             "desc": "Column:",
             "widget": widgets.Dropdown(
                 options=[z for z in COLUMNS if z in df.columns],
-                layout=Layout(width=WIDGET_WIDTH),
+                layout=Layout(width="95%"),
             ),
         },
         # 1
         {
             "arg": "main_term",
             "desc": "Term in Column:",
-            "widget": widgets.Dropdown(options=[], layout=Layout(width=WIDGET_WIDTH),),
+            "widget": widgets.Dropdown(options=[], layout=Layout(width="95%"),),
         },
         # 2
         {
@@ -254,20 +252,22 @@ def matrix(df, top_n=50):
             "desc": "By column:",
             "widget": widgets.Dropdown(
                 options=[z for z in COLUMNS if z in df.columns],
-                layout=Layout(width=WIDGET_WIDTH),
+                layout=Layout(width="90%"),
             ),
         },
         # 3
         {
             "arg": "by_term",
             "desc": "Term in By column:",
-            "widget": widgets.Dropdown(options=[], layout=Layout(width=WIDGET_WIDTH),),
+            "widget": widgets.Dropdown(options=[], layout=Layout(width="95%"),),
         },
         # 4
         {
             "arg": "title",
             "desc": "Title:",
-            "widget": widgets.Select(options=[], layout=Layout(width="500px"),),
+            "widget": widgets.Select(
+                options=[], layout=Layout(height="320pt", width="95%"),
+            ),
         },
     ]
     # -------------------------------------------------------------------------
@@ -301,32 +301,32 @@ def matrix(df, top_n=50):
                 summary.sort_values("Times_Cited", ascending=False).head(top_n).index
             )
             top_terms = sorted(top_terms_freq | top_terms_cited_by)
-            controls[1]["widget"].options = top_terms
+            left_panel[1]["widget"].options = top_terms
         else:
             top_terms = pd.Series(xdf["_key1_"].unique())
             top_terms = top_terms[top_terms.map(lambda w: not pd.isna(w))]
             top_terms = top_terms.sort_values()
-            controls[1]["widget"].options = top_terms
+            left_panel[1]["widget"].options = top_terms
         #
         # Subset selection
         #
         if by_column in MULTIVALUED_COLS:
             xdf = __explode(xdf, "_key2_")
-        xdf = xdf[xdf["_key1_"] == controls[1]["widget"].value]
+        xdf = xdf[xdf["_key1_"] == left_panel[1]["widget"].value]
         terms = sorted(pd.Series(xdf["_key2_"].dropna().unique()))
-        controls[3]["widget"].options = terms
+        left_panel[3]["widget"].options = terms
         #
         # Title
         #
-        xdf = xdf[xdf["_key2_"] == controls[3]["widget"].value]
+        xdf = xdf[xdf["_key2_"] == left_panel[3]["widget"].value]
         if len(xdf):
-            controls[4]["widget"].options = sorted(xdf["Title"].tolist())
+            left_panel[4]["widget"].options = sorted(xdf["Title"].tolist())
         else:
-            controls[4]["widget"].options = []
+            left_panel[4]["widget"].options = []
         #
         # Print info from selected title
         #
-        out = df[df["Title"] == controls[4]["widget"].value]
+        out = df[df["Title"] == left_panel[4]["widget"].value]
         out = out.reset_index(drop=True)
         out = out.iloc[0]
         output.clear_output()
@@ -339,39 +339,31 @@ def matrix(df, top_n=50):
     # Generic
     #
     # -------------------------------------------------------------------------
-    args = {control["arg"]: control["widget"] for control in controls}
+    args = {control["arg"]: control["widget"] for control in left_panel}
     output = widgets.Output()
     with output:
         display(widgets.interactive_output(server, args,))
     #
     #
-    grid = GridspecLayout(10, 5, height="800px")
+    grid = GridspecLayout(10, 4, height="800px")
     grid[0, :] = widgets.HTML(
         value="<h1>{}</h1><hr style='height:2px;border-width:0;color:gray;background-color:gray'>".format(
             "Matrix Explorer (Top {} terms)".format(top_n)
         )
     )
-    grid[1, 0] = widgets.VBox(
-        [widgets.Label(value=controls[0]["desc"]), controls[0]["widget"],]
+    for i in range(0, len(left_panel) - 1):
+        grid[i + 1, 0] = widgets.VBox(
+            [widgets.Label(value=left_panel[i]["desc"]), left_panel[i]["widget"],]
+        )
+
+    grid[len(left_panel) - 1 : len(left_panel) + 5, 0] = widgets.VBox(
+        [widgets.Label(value=left_panel[-1]["desc"]), left_panel[-1]["widget"],]
     )
 
-    grid[2, 0] = widgets.VBox(
-        [widgets.Label(value=controls[1]["desc"]), controls[1]["widget"]]
+    grid[1:, 1:] = widgets.VBox(
+        [output],  #  layout=Layout(height="657px", border="2px solid gray")
+        layout=Layout(border="2px solid gray"),
     )
-
-    grid[1, 1] = widgets.VBox(
-        [widgets.Label(value=controls[2]["desc"]), controls[2]["widget"],]
-    )
-
-    grid[2, 1] = widgets.VBox(
-        [widgets.Label(value=controls[3]["desc"]), controls[3]["widget"]]
-    )
-
-    grid[1:3, 2:] = widgets.VBox(
-        [widgets.Label(value=controls[4]["desc"]), controls[4]["widget"]]
-    )
-
-    grid[3:, :] = widgets.VBox([output])
 
     return grid
 
