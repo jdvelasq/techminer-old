d325ba5f3c87c910c9a9310cf1361289d25040da
jdvelasq <jdvelasq@unal.edu.co>
2020-07-07 13:30:42 +0000

    term-analysis gui

--- techminer/by_term.py
+++ techminer/by_term.py
@@ -504,33 +504,35 @@ def __TAB0__(data, limit_to, exclude):
     # Generic
     #
     # -------------------------------------------------------------------------
-    args = {control["arg"]: control["widget"] for control in left_panel}
+    # args = {control["arg"]: control["widget"] for control in left_panel}
+    # output = widgets.Output()
+    # with output:
+    #     display(widgets.interactive_output(server, args))
+
+    # grid = GridspecLayout(13, 6, height="650px")
+    # #
+    # # Left panel
+    # #
+    # for index in range(len(left_panel)):
+    #     grid[index, 0] = widgets.HBox(
+    #         [
+    #             widgets.Label(value=left_panel[index]["desc"]),
+    #             left_panel[index]["widget"],
+    #         ],
+    #         layout=Layout(
+    #             display="flex", justify_content="flex-end", align_content="center",
+    #         ),
+    #     )
+    # #
+    # # Output
+    # #
+    # grid[0:, 1:] = widgets.VBox(
+    #     [output], layout=Layout(height="650px", border="2px solid gray")
+    # )
+
+    ###
     output = widgets.Output()
-    with output:
-        display(widgets.interactive_output(server, args))
-
-    grid = GridspecLayout(13, 6, height="650px")
-    #
-    # Left panel
-    #
-    for index in range(len(left_panel)):
-        grid[index, 0] = widgets.HBox(
-            [
-                widgets.Label(value=left_panel[index]["desc"]),
-                left_panel[index]["widget"],
-            ],
-            layout=Layout(
-                display="flex", justify_content="flex-end", align_content="center",
-            ),
-        )
-    #
-    # Output
-    #
-    grid[0:, 1:] = widgets.VBox(
-        [output], layout=Layout(height="650px", border="2px solid gray")
-    )
-
-    return grid
+    return gui.TABapp(left_panel=left_panel, server=server, output=output)
 
 
 #
