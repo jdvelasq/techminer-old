96a50ad2eaa64fb1c31ff6b5c96250d7c492ed88
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-07-29 19:20:52 +0000

    factor-analysis review

--- techminer/factor_analysis.py
+++ techminer/factor_analysis.py
@@ -21,6 +21,7 @@ import techminer.common as cmn
 import techminer.dashboard as dash
 from techminer.dashboard import DASH
 from techminer.document_term import TF_matrix, TFIDF_matrix
+from techminer.graph import network_normalization
 
 ###############################################################################
 ##
@@ -29,225 +30,225 @@ from techminer.document_term import TF_matrix, TFIDF_matrix
 ###############################################################################
 
 
-class Model:
-    def __init__(self, data, limit_to, exclude):
-        #
-        self.data = data
-        self.limit_to = limit_to
-        self.exclude = exclude
-        #
-        self.ascending = None
-        self.cmap = None
-        self.column = None
-        self.height = None
-        self.iterations = None
-        self.layout = None
-        self.n_components = None
-        self.random_state = None
-        self.sort_by = None
-        self.top_by = None
-        self.top_n = None
-        self.width = None
+# class Model:
+#     def __init__(self, data, limit_to, exclude):
+#         #
+#         self.data = data
+#         self.limit_to = limit_to
+#         self.exclude = exclude
+#         #
+#         self.ascending = None
+#         self.cmap = None
+#         self.column = None
+#         self.height = None
+#         self.iterations = None
+#         self.layout = None
+#         self.n_components = None
+#         self.random_state = None
+#         self.sort_by = None
+#         self.top_by = None
+#         self.top_n = None
+#         self.width = None
 
-    def sort(self):
+#     def sort(self):
 
-        R = self.factors_.copy()
+#         R = self.factors_.copy()
 
-        if self.top_by == "Values":
+#         if self.top_by == "Values":
 
-            m = R.copy()
-            m = m.applymap(abs)
-            m = m.max(axis=1)
-            m = m.sort_values(ascending=False)
-            m = m.head(self.top_n)
-            m = m.index
-            R = R.loc[m, :]
+#             m = R.copy()
+#             m = m.applymap(abs)
+#             m = m.max(axis=1)
+#             m = m.sort_values(ascending=False)
+#             m = m.head(self.top_n)
+#             m = m.index
+#             R = R.loc[m, :]
 
-        else:
+#         else:
 
-            R = cmn.sort_by_axis(data=R, sort_by=self.top_by, ascending=False, axis=0)
-            R = R.head(self.top_n)
+#             R = cmn.sort_by_axis(data=R, sort_by=self.top_by, ascending=False, axis=0)
+#             R = R.head(self.top_n)
 
-        if self.sort_by in ["Alphabetic", "Num Documents", "Times Cited"]:
-            R = cmn.sort_by_axis(
-                data=R, sort_by=self.sort_by, ascending=self.ascending, axis=0
-            )
+#         if self.sort_by in ["Alphabetic", "Num Documents", "Times Cited"]:
+#             R = cmn.sort_by_axis(
+#                 data=R, sort_by=self.sort_by, ascending=self.ascending, axis=0
+#             )
 
-        if self.sort_by in ["F{}".format(i) for i in range(len(R.columns))]:
-            R = R.sort_values(self.sort_by, ascending=self.ascending)
+#         if self.sort_by in ["F{}".format(i) for i in range(len(R.columns))]:
+#             R = R.sort_values(self.sort_by, ascending=self.ascending)
 
-        if len(R) == 0:
-            return None
+#         if len(R) == 0:
+#             return None
 
-        return R
+#         return R
 
-    def factors(self):
-        self.fit()
-        output = self.sort()
-        if self.cmap is None:
-            return output
-        else:
-            return output.style.background_gradient(cmap=self.cmap)
+#     def factors(self):
+#         self.fit()
+#         output = self.sort()
+#         if self.cmap is None:
+#             return output
+#         else:
+#             return output.style.background_gradient(cmap=self.cmap)
 
-    def variances(self):
-        self.fit()
-        return self.variances_
+#     def variances(self):
+#         self.fit()
+#         return self.variances_
 
-    def map(self):
-        self.fit()
-        X = self.factors_.copy()
+#     def map(self):
+#         self.fit()
+#         X = self.factors_.copy()
 
-        if self.top_by == "Values":
+#         if self.top_by == "Values":
 
-            m = X.copy()
-            m = m.applymap(abs)
-            m = m.max(axis=1)
-            m = m.sort_values(ascending=False)
-            m = m.head(self.top_n)
-            m = m.index
-            X = X.loc[m, :]
+#             m = X.copy()
+#             m = m.applymap(abs)
+#             m = m.max(axis=1)
+#             m = m.sort_values(ascending=False)
+#             m = m.head(self.top_n)
+#             m = m.index
+#             X = X.loc[m, :]
 
-        else:
+#         else:
 
-            X = cmn.sort_by_axis(data=X, sort_by=self.top_by, ascending=False, axis=0)
-            X = X.head(self.top_n)
+#             X = cmn.sort_by_axis(data=X, sort_by=self.top_by, ascending=False, axis=0)
+#             X = X.head(self.top_n)
 
-        ## Networkx
-        fig = pyplot.Figure(figsize=(self.width, self.height))
-        ax = fig.subplots()
-        G = nx.Graph(ax=ax)
-        G.clear()
+#         ## Networkx
+#         fig = pyplot.Figure(figsize=(self.width, self.height))
+#         ax = fig.subplots()
+#         G = nx.Graph(ax=ax)
+#         G.clear()
 
-        ## Data preparation
-        terms = X.index.tolist()
-        node_sizes = cmn.counters_to_node_sizes(x=terms)
-        node_colors = cmn.counters_to_node_colors(
-            x=terms, cmap=pyplot.cm.get_cmap(self.cmap)
-        )
+#         ## Data preparation
+#         terms = X.index.tolist()
+#         node_sizes = cmn.counters_to_node_sizes(x=terms)
+#         node_colors = cmn.counters_to_node_colors(
+#             x=terms, cmap=pyplot.cm.get_cmap(self.cmap)
+#         )
 
-        ## Add nodes
-        G.add_nodes_from(terms)
+#         ## Add nodes
+#         G.add_nodes_from(terms)
 
-        ## node positions
-        if self.layout == "Spring":
-            pos = nx.spring_layout(G, iterations=self.iterations)
-        else:
-            pos = {
-                "Circular": nx.circular_layout,
-                "Kamada Kawai": nx.kamada_kawai_layout,
-                "Planar": nx.planar_layout,
-                "Random": nx.random_layout,
-                "Spectral": nx.spectral_layout,
-                "Spring": nx.spring_layout,
-                "Shell": nx.shell_layout,
-            }[self.layout](G)
+#         ## node positions
+#         if self.layout == "Spring":
+#             pos = nx.spring_layout(G, iterations=self.iterations)
+#         else:
+#             pos = {
+#                 "Circular": nx.circular_layout,
+#                 "Kamada Kawai": nx.kamada_kawai_layout,
+#                 "Planar": nx.planar_layout,
+#                 "Random": nx.random_layout,
+#                 "Spectral": nx.spectral_layout,
+#                 "Spring": nx.spring_layout,
+#                 "Shell": nx.shell_layout,
+#             }[self.layout](G)
 
-        d = {
-            0: {"width": 4, "style": "solid", "edge_color": "k"},
-            1: {"width": 2, "style": "solid", "edge_color": "k"},
-            2: {"width": 1, "style": "dashed", "edge_color": "gray"},
-            3: {"width": 1, "style": "dotted", "edge_color": "gray"},
-        }
+#         d = {
+#             0: {"width": 4, "style": "solid", "edge_color": "k"},
+#             1: {"width": 2, "style": "solid", "edge_color": "k"},
+#             2: {"width": 1, "style": "dashed", "edge_color": "gray"},
+#             3: {"width": 1, "style": "dotted", "edge_color": "gray"},
+#         }
 
-        n_edges_0 = 0
-        n_edges_25 = 0
-        n_edges_50 = 0
-        n_edges_75 = 0
+#         n_edges_0 = 0
+#         n_edges_25 = 0
+#         n_edges_50 = 0
+#         n_edges_75 = 0
 
-        for factor in X.columns:
+#         for factor in X.columns:
 
-            for k in [0, 1]:
+#             for k in [0, 1]:
 
-                M = X[[factor]]
+#                 M = X[[factor]]
 
-                if k == 1:
-                    M = M.applymap(lambda w: -w)
+#                 if k == 1:
+#                     M = M.applymap(lambda w: -w)
 
-                M = M[M[factor] >= 0.25]
+#                 M = M[M[factor] >= 0.25]
 
-                if len(M) > 0:
-                    F = M[[factor]].values.T + M[[factor]].values
-                    F = F / 2
-                    F = pd.DataFrame(F, columns=M.index, index=M.index)
-                    m = F.stack().to_frame().reset_index()
-                    m = m[m.level_0 < m.level_1]
-                    m.columns = ["from_", "to_", "link_"]
-                    m = m.reset_index(drop=True)
+#                 if len(M) > 0:
+#                     F = M[[factor]].values.T + M[[factor]].values
+#                     F = F / 2
+#                     F = pd.DataFrame(F, columns=M.index, index=M.index)
+#                     m = F.stack().to_frame().reset_index()
+#                     m = m[m.level_0 < m.level_1]
+#                     m.columns = ["from_", "to_", "link_"]
+#                     m = m.reset_index(drop=True)
 
-                    for idx in range(len(m)):
+#                     for idx in range(len(m)):
 
-                        edge = [(m.from_[idx], m.to_[idx])]
-                        key = (
-                            0
-                            if m.link_[idx] > 0.75
-                            else (
-                                1
-                                if m.link_[idx] > 0.50
-                                else (2 if m.link_[idx] > 0.25 else 3)
-                            )
-                        )
+#                         edge = [(m.from_[idx], m.to_[idx])]
+#                         key = (
+#                             0
+#                             if m.link_[idx] > 0.75
+#                             else (
+#                                 1
+#                                 if m.link_[idx] > 0.50
+#                                 else (2 if m.link_[idx] > 0.25 else 3)
+#                             )
+#                         )
 
-                        n_edges_75 += 1 if m.link_[idx] >= 0.75 else 0
-                        n_edges_50 += (
-                            1 if m.link_[idx] >= 0.50 and m.link_[idx] < 0.75 else 0
-                        )
-                        n_edges_25 += (
-                            1 if m.link_[idx] >= 0.25 and m.link_[idx] < 0.50 else 0
-                        )
-                        n_edges_0 += (
-                            1 if m.link_[idx] > 0 and m.link_[idx] < 0.25 else 0
-                        )
+#                         n_edges_75 += 1 if m.link_[idx] >= 0.75 else 0
+#                         n_edges_50 += (
+#                             1 if m.link_[idx] >= 0.50 and m.link_[idx] < 0.75 else 0
+#                         )
+#                         n_edges_25 += (
+#                             1 if m.link_[idx] >= 0.25 and m.link_[idx] < 0.50 else 0
+#                         )
+#                         n_edges_0 += (
+#                             1 if m.link_[idx] > 0 and m.link_[idx] < 0.25 else 0
+#                         )
 
-                        nx.draw_networkx_edges(
-                            G,
-                            pos=pos,
-                            ax=ax,
-                            node_size=1,
-                            with_labels=False,
-                            edgelist=edge,
-                            **(d[key])
-                        )
+#                         nx.draw_networkx_edges(
+#                             G,
+#                             pos=pos,
+#                             ax=ax,
+#                             node_size=1,
+#                             with_labels=False,
+#                             edgelist=edge,
+#                             **(d[key])
+#                         )
 
-        ## nodes
-        nx.draw_networkx_nodes(
-            G,
-            pos=pos,
-            ax=ax,
-            edge_color="k",
-            nodelist=terms,
-            node_size=node_sizes,
-            node_color=node_colors,
-            node_shape="o",
-            edgecolors="k",
-            linewidths=1,
-        )
+#         ## nodes
+#         nx.draw_networkx_nodes(
+#             G,
+#             pos=pos,
+#             ax=ax,
+#             edge_color="k",
+#             nodelist=terms,
+#             node_size=node_sizes,
+#             node_color=node_colors,
+#             node_shape="o",
+#             edgecolors="k",
+#             linewidths=1,
+#         )
 
-        ## node labels
-        cmn.ax_text_node_labels(
-            ax=ax, labels=terms, dict_pos=pos, node_sizes=node_sizes
-        )
+#         ## node labels
+#         cmn.ax_text_node_labels(
+#             ax=ax, labels=terms, dict_pos=pos, node_sizes=node_sizes
+#         )
 
-        ## Figure size
-        cmn.ax_expand_limits(ax)
+#         ## Figure size
+#         cmn.ax_expand_limits(ax)
 
-        ##
-        legend_lines = [
-            Line2D([0], [0], color="k", linewidth=4, linestyle="-"),
-            Line2D([0], [0], color="k", linewidth=2, linestyle="-"),
-            Line2D([0], [0], color="gray", linewidth=1, linestyle="--"),
-            Line2D([0], [0], color="gray", linewidth=1, linestyle=":"),
-        ]
+#         ##
+#         legend_lines = [
+#             Line2D([0], [0], color="k", linewidth=4, linestyle="-"),
+#             Line2D([0], [0], color="k", linewidth=2, linestyle="-"),
+#             Line2D([0], [0], color="gray", linewidth=1, linestyle="--"),
+#             Line2D([0], [0], color="gray", linewidth=1, linestyle=":"),
+#         ]
 
-        text_75 = "> 0.75 ({})".format(n_edges_75)
-        text_50 = "0.50-0.75 ({})".format(n_edges_50)
-        text_25 = "0.25-0.50 ({})".format(n_edges_25)
-        text_0 = "< 0.25 ({})".format(n_edges_0)
+#         text_75 = "> 0.75 ({})".format(n_edges_75)
+#         text_50 = "0.50-0.75 ({})".format(n_edges_50)
+#         text_25 = "0.25-0.50 ({})".format(n_edges_25)
+#         text_0 = "< 0.25 ({})".format(n_edges_0)
 
-        ax.legend(legend_lines, [text_75, text_50, text_25, text_0])
+#         ax.legend(legend_lines, [text_75, text_50, text_25, text_0])
 
-        ax.axis("off")
+#         ax.axis("off")
 
-        return fig
+#         return fig
 
 
 ###############################################################################
@@ -296,7 +297,7 @@ class DASHapp(DASH):
         self.top_n = None
         self.width = None
         #
-        self.app_title = "Latent Semantic Analysis"
+        self.app_title = "Factor Analysis"
         self.menu_options = [
             "Memberships",
             "Cluster plot",
@@ -308,8 +309,8 @@ class DASHapp(DASH):
             ),
             dash.min_occurrence(),
             dash.max_items(),
+            dash.normalization(),
             dash.separator(text="Decomposition"),
-            dash.dropdown(desc="Apply to:", options=["TF matrix", "TF*IDF matrix",],),
             dash.dropdown(
                 desc="Method:",
                 options=["Factor Analysis", "PCA", "Fast ICA", "SVD", "MDS"],
@@ -356,28 +357,27 @@ class DASHapp(DASH):
         #
         # 1.-- TF matrix
         #
-        M = TF_matrix(
+        TF_matrix_ = TF_matrix(
             data=X, column=self.column, scheme=None, min_occurrence=self.min_occurrence,
         )
 
         #
-        # 2.-- Computtes TFIDF matrix and select max_term frequent terms
+        # 2.-- Co-occurrence matrix and normalization
         #
         #      tf-idf = tf * (log(N / df) + 1)
         #
-        if self.apply_to == "TF*IDF matrix":
-            M = TFIDF_matrix(
-                TF_matrix=M,
-                norm=None,
-                use_idf=False,
-                smooth_idf=False,
-                sublinear_tf=False,
-                max_items=self.max_items,
-            )
+        TF_matrix_ = cmn.add_counters_to_axis(
+            X=TF_matrix_, axis=1, data=self.data, column=self.column
+        )
+
+        M = np.matmul(TF_matrix_.transpose().values, TF_matrix_.values)
+        M = pd.DataFrame(M, columns=TF_matrix_.columns, index=TF_matrix_.columns)
+        M = network_normalization(M, normalization=self.normalization)
 
         #
         # 3.-- Add counters to axes
         #
+        M = cmn.add_counters_to_axis(X=M, axis=0, data=self.data, column=self.column)
         M = cmn.add_counters_to_axis(X=M, axis=1, data=self.data, column=self.column)
 
         #
@@ -410,7 +410,6 @@ class DASHapp(DASH):
             limit_to=self.limit_to,
             exclude=self.exclude,
         )
-        # R = cmn.add_counters_to_axis(X=R, axis=0, data=X, column=self.column)
 
         #
         # 6.-- Clustering
