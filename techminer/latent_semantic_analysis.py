3196784194a6607da60497f9d6c8fb45d11577a8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-08-01 20:18:59 +0000

    MVC latent-semantic-analysis

--- techminer/latent_semantic_analysis.py
+++ techminer/latent_semantic_analysis.py
@@ -29,312 +29,25 @@ from techminer.document_term import TF_matrix, TFIDF_matrix
 ###############################################################################
 
 
-# class Model:
-#     def __init__(self, data, limit_to, exclude):
-#         #
-#         self.data = data
-#         self.limit_to = limit_to
-#         self.exclude = exclude
-#         #
-#         self.ascending = None
-#         self.cmap = None
-#         self.column = None
-#         self.height = None
-#         self.iterations = None
-#         self.layout = None
-#         self.n_components = None
-#         self.random_state = None
-#         self.sort_by = None
-#         self.top_by = None
-#         self.top_n = None
-#         self.width = None
-
-#     def sort(self):
-
-#         R = self.factors_.copy()
-
-#         if self.top_by == "Values":
-
-#             m = R.copy()
-#             m = m.applymap(abs)
-#             m = m.max(axis=1)
-#             m = m.sort_values(ascending=False)
-#             m = m.head(self.top_n)
-#             m = m.index
-#             R = R.loc[m, :]
-
-#         else:
-
-#             R = cmn.sort_by_axis(data=R, sort_by=self.top_by, ascending=False, axis=0)
-#             R = R.head(self.top_n)
-
-#         if self.sort_by in ["Alphabetic", "Num Documents", "Times Cited"]:
-#             R = cmn.sort_by_axis(
-#                 data=R, sort_by=self.sort_by, ascending=self.ascending, axis=0
-#             )
-
-#         if self.sort_by in ["F{}".format(i) for i in range(len(R.columns))]:
-#             R = R.sort_values(self.sort_by, ascending=self.ascending)
-
-#         if len(R) == 0:
-#             return None
-
-#         return R
-
-#     def factors(self):
-#         self.fit()
-#         output = self.sort()
-#         if self.cmap is None:
-#             return output
-#         else:
-#             return output.style.background_gradient(cmap=self.cmap)
-
-#     def variances(self):
-#         self.fit()
-#         return self.variances_
-
-#     def map(self):
-#         self.fit()
-#         X = self.factors_.copy()
-
-#         if self.top_by == "Values":
-
-#             m = X.copy()
-#             m = m.applymap(abs)
-#             m = m.max(axis=1)
-#             m = m.sort_values(ascending=False)
-#             m = m.head(self.top_n)
-#             m = m.index
-#             X = X.loc[m, :]
-
-#         else:
-
-#             X = cmn.sort_by_axis(data=X, sort_by=self.top_by, ascending=False, axis=0)
-#             X = X.head(self.top_n)
-
-#         ## Networkx
-#         fig = pyplot.Figure(figsize=(self.width, self.height))
-#         ax = fig.subplots()
-#         G = nx.Graph(ax=ax)
-#         G.clear()
-
-#         ## Data preparation
-#         terms = X.index.tolist()
-#         node_sizes = cmn.counters_to_node_sizes(x=terms)
-#         node_colors = cmn.counters_to_node_colors(
-#             x=terms, cmap=pyplot.cm.get_cmap(self.cmap)
-#         )
-
-#         ## Add nodes
-#         G.add_nodes_from(terms)
-
-#         ## node positions
-#         if self.layout == "Spring":
-#             pos = nx.spring_layout(G, iterations=self.iterations)
-#         else:
-#             pos = {
-#                 "Circular": nx.circular_layout,
-#                 "Kamada Kawai": nx.kamada_kawai_layout,
-#                 "Planar": nx.planar_layout,
-#                 "Random": nx.random_layout,
-#                 "Spectral": nx.spectral_layout,
-#                 "Spring": nx.spring_layout,
-#                 "Shell": nx.shell_layout,
-#             }[self.layout](G)
-
-#         d = {
-#             0: {"width": 4, "style": "solid", "edge_color": "k"},
-#             1: {"width": 2, "style": "solid", "edge_color": "k"},
-#             2: {"width": 1, "style": "dashed", "edge_color": "gray"},
-#             3: {"width": 1, "style": "dotted", "edge_color": "gray"},
-#         }
-
-#         n_edges_0 = 0
-#         n_edges_25 = 0
-#         n_edges_50 = 0
-#         n_edges_75 = 0
-
-#         for factor in X.columns:
-
-#             for k in [0, 1]:
-
-#                 M = X[[factor]]
-
-#                 if k == 1:
-#                     M = M.applymap(lambda w: -w)
-
-#                 M = M[M[factor] >= 0.25]
-
-#                 if len(M) > 0:
-#                     F = M[[factor]].values.T + M[[factor]].values
-#                     F = F / 2
-#                     F = pd.DataFrame(F, columns=M.index, index=M.index)
-#                     m = F.stack().to_frame().reset_index()
-#                     m = m[m.level_0 < m.level_1]
-#                     m.columns = ["from_", "to_", "link_"]
-#                     m = m.reset_index(drop=True)
-
-#                     for idx in range(len(m)):
-
-#                         edge = [(m.from_[idx], m.to_[idx])]
-#                         key = (
-#                             0
-#                             if m.link_[idx] > 0.75
-#                             else (
-#                                 1
-#                                 if m.link_[idx] > 0.50
-#                                 else (2 if m.link_[idx] > 0.25 else 3)
-#                             )
-#                         )
-
-#                         n_edges_75 += 1 if m.link_[idx] >= 0.75 else 0
-#                         n_edges_50 += (
-#                             1 if m.link_[idx] >= 0.50 and m.link_[idx] < 0.75 else 0
-#                         )
-#                         n_edges_25 += (
-#                             1 if m.link_[idx] >= 0.25 and m.link_[idx] < 0.50 else 0
-#                         )
-#                         n_edges_0 += (
-#                             1 if m.link_[idx] > 0 and m.link_[idx] < 0.25 else 0
-#                         )
-
-#                         nx.draw_networkx_edges(
-#                             G,
-#                             pos=pos,
-#                             ax=ax,
-#                             node_size=1,
-#                             with_labels=False,
-#                             edgelist=edge,
-#                             **(d[key])
-#                         )
-
-#         ## nodes
-#         nx.draw_networkx_nodes(
-#             G,
-#             pos=pos,
-#             ax=ax,
-#             edge_color="k",
-#             nodelist=terms,
-#             node_size=node_sizes,
-#             node_color=node_colors,
-#             node_shape="o",
-#             edgecolors="k",
-#             linewidths=1,
-#         )
-
-#         ## node labels
-#         cmn.ax_text_node_labels(
-#             ax=ax, labels=terms, dict_pos=pos, node_sizes=node_sizes
-#         )
-
-#         ## Figure size
-#         cmn.ax_expand_limits(ax)
-
-#         ##
-#         legend_lines = [
-#             Line2D([0], [0], color="k", linewidth=4, linestyle="-"),
-#             Line2D([0], [0], color="k", linewidth=2, linestyle="-"),
-#             Line2D([0], [0], color="gray", linewidth=1, linestyle="--"),
-#             Line2D([0], [0], color="gray", linewidth=1, linestyle=":"),
-#         ]
-
-#         text_75 = "> 0.75 ({})".format(n_edges_75)
-#         text_50 = "0.50-0.75 ({})".format(n_edges_50)
-#         text_25 = "0.25-0.50 ({})".format(n_edges_25)
-#         text_0 = "< 0.25 ({})".format(n_edges_0)
-
-#         ax.legend(legend_lines, [text_75, text_50, text_25, text_0])
-
-#         ax.axis("off")
-
-#         return fig
-
-
 ###############################################################################
 ##
-##  DASHBOARD
+##  MODEL
 ##
 ###############################################################################
 
-COLUMNS = [
-    "Authors",
-    "Countries",
-    "Institutions",
-    "Author_Keywords",
-    "Index_Keywords",
-    "Abstract_words_CL",
-    "Abstract_words",
-    "Title_words_CL",
-    "Title_words",
-    "Affiliations",
-    "Author_Keywords_CL",
-    "Index_Keywords_CL",
-]
 
+class Model:
+    def __init__(self, data, limit_to, exclude, years_range):
+        ##
+        if years_range is not None:
+            initial_year, final_year = years_range
+            data = data[(data.Year >= initial_year) & (data.Year <= final_year)]
 
-class DASHapp(DASH):
-    def __init__(self, data, limit_to=None, exclude=None, years_range=None):
-        """Dashboard app"""
+        self.data = data
+        self.limit_to = limit_to
+        self.exclude = exclude
 
-        DASH.__init__(
-            self, data=data, limit_to=limit_to, exclude=exclude, years_range=years_range
-        )
-
-        #
-        self.app_title = "Latent Semantic Analysis"
-        self.menu_options = [
-            "Memberships",
-            "Cluster plot",
-        ]
-        #
-        self.panel_widgets = [
-            dash.dropdown(
-                desc="Column:", options=[z for z in COLUMNS if z in data.columns],
-            ),
-            dash.min_occurrence(),
-            dash.max_items(),
-            dash.separator(text="Decomposition"),
-            dash.dropdown(desc="Apply to:", options=["TF matrix", "TF*IDF matrix",],),
-            dash.dropdown(
-                desc="Method:",
-                options=["Factor Analysis", "PCA", "Fast ICA", "SVD", "MDS"],
-            ),
-            dash.n_components(),
-            dash.random_state(),
-            dash.separator(text="Aglomerative Clustering"),
-            dash.n_clusters(),
-            dash.affinity(),
-            dash.linkage(),
-            dash.separator(text="Visualization"),
-            dash.dropdown(desc="Top by:", options=["Num Documents", "Times Cited"],),
-            dash.top_n(),
-            dash.x_axis(),
-            dash.y_axis(),
-            dash.fig_width(),
-            dash.fig_height(),
-        ]
-        super().create_grid()
-
-    def interactive_output(self, **kwargs):
-
-        DASH.interactive_output(self, **kwargs)
-
-        if self.menu == "Memberships":
-            self.set_disabled("X-axis:")
-            self.set_disabled("Y-axis:")
-            self.set_disabled("Width:")
-            self.set_disabled("Height:")
-
-        if self.menu == "Cluster plot":
-            self.set_enabled("X-axis:")
-            self.set_enabled("Y-axis:")
-            self.set_enabled("Width:")
-            self.set_enabled("Height:")
-
-        self.set_options(name="X-axis:", options=list(range(self.n_components)))
-        self.set_options(name="Y-axis:", options=list(range(self.n_components)))
-
-    def fit(self):
+    def apply(self):
         #
         X = self.data.copy()
 
@@ -435,7 +148,7 @@ class DASHapp(DASH):
 
     def memberships(self):
         ##
-        self.fit()
+        self.apply()
         ##
         HTML = ""
         for i_cluster in range(self.n_clusters):
@@ -459,7 +172,7 @@ class DASHapp(DASH):
 
     def cluster_plot(self):
         ##
-        self.fit()
+        self.apply()
         ##
 
         fig = pyplot.Figure(figsize=(self.width, self.height))
@@ -500,6 +213,98 @@ class DASHapp(DASH):
         fig.set_tight_layout(True)
 
         return fig
+
+
+###############################################################################
+##
+##  DASHBOARD
+##
+###############################################################################
+
+COLUMNS = [
+    "Authors",
+    "Countries",
+    "Institutions",
+    "Author_Keywords",
+    "Index_Keywords",
+    "Abstract_words_CL",
+    "Abstract_words",
+    "Title_words_CL",
+    "Title_words",
+    "Affiliations",
+    "Author_Keywords_CL",
+    "Index_Keywords_CL",
+]
+
+###############################################################################
+##
+##  DASHBOARD
+##
+###############################################################################
+
+
+class DASHapp(DASH, Model):
+    def __init__(self, data, limit_to=None, exclude=None, years_range=None):
+        """Dashboard app"""
+
+        Model.__init__(
+            self, data=data, limit_to=limit_to, exclude=exclude, years_range=years_range
+        )
+        DASH.__init__(self)
+
+        #
+        self.app_title = "Latent Semantic Analysis"
+        self.menu_options = [
+            "Memberships",
+            "Cluster plot",
+        ]
+        #
+        self.panel_widgets = [
+            dash.dropdown(
+                desc="Column:", options=[z for z in COLUMNS if z in data.columns],
+            ),
+            dash.min_occurrence(),
+            dash.max_items(),
+            dash.separator(text="Decomposition"),
+            dash.dropdown(desc="Apply to:", options=["TF matrix", "TF*IDF matrix",],),
+            dash.dropdown(
+                desc="Method:",
+                options=["Factor Analysis", "PCA", "Fast ICA", "SVD", "MDS"],
+            ),
+            dash.n_components(),
+            dash.random_state(),
+            dash.separator(text="Aglomerative Clustering"),
+            dash.n_clusters(),
+            dash.affinity(),
+            dash.linkage(),
+            dash.separator(text="Visualization"),
+            dash.dropdown(desc="Top by:", options=["Num Documents", "Times Cited"],),
+            dash.top_n(),
+            dash.x_axis(),
+            dash.y_axis(),
+            dash.fig_width(),
+            dash.fig_height(),
+        ]
+        super().create_grid()
+
+    def interactive_output(self, **kwargs):
+
+        DASH.interactive_output(self, **kwargs)
+
+        if self.menu == "Memberships":
+            self.set_disabled("X-axis:")
+            self.set_disabled("Y-axis:")
+            self.set_disabled("Width:")
+            self.set_disabled("Height:")
+
+        if self.menu == "Cluster plot":
+            self.set_enabled("X-axis:")
+            self.set_enabled("Y-axis:")
+            self.set_enabled("Width:")
+            self.set_enabled("Height:")
+
+        self.set_options(name="X-axis:", options=list(range(self.n_components)))
+        self.set_options(name="Y-axis:", options=list(range(self.n_components)))
 
 
 ###############################################################################
