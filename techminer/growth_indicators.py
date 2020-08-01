455ddecb58f8991398192fb60e66263604086f38
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-08-01 19:53:14 +0000

    MVC growth-indicators

--- techminer/growth_indicators.py
+++ techminer/growth_indicators.py
@@ -8,168 +8,25 @@ from techminer.by_term_per_year import _build_table
 from techminer.dashboard import DASH
 
 
-def growth_indicators(
-    x,
-    column,
-    time_window=2,
-    output=0,
-    top_by=None,
-    top_n=None,
-    sort_by=None,
-    ascending=True,
-    plot=None,
-    cmap="Greys",
-    figsize=(5, 5),
-    limit_to=None,
-    exclude=None,
-):
-    """Computes the average growth rate of a group of terms.
-
-    Args:
-        column (str): the column to explode.
-        time_window (int): time window for analysis
-        keywords (Keywords): filter the result using the specified Keywords object.
-
-    Returns:
-        DataFrame.
-
-
-    Examples
-    ----------------------------------------------------------------------------------------------
-
-    >>> import pandas as pd
-    >>> x = pd.DataFrame(
-    ...   {
-    ...     "Year": [2010, 2010, 2011, 2011, 2012, 2013, 2014, 2014],
-    ...     "Authors": "author 0;author 1;author 2,author 0,author 1,author 3,author 4,author 4,author 0;author 3,author 3;author 4".split(","),
-    ...     "Times_Cited": list(range(10,18)),
-    ...     "ID": list(range(8)),
-    ...   }
-    ... )
-    >>> x
-       Year                     Authors  Times_Cited  ID
-    0  2010  author 0;author 1;author 2           10   0
-    1  2010                    author 0           11   1
-    2  2011                    author 1           12   2
-    3  2011                    author 3           13   3
-    4  2012                    author 4           14   4
-    5  2013                    author 4           15   5
-    6  2014           author 0;author 3           16   6
-    7  2014           author 3;author 4           17   7
-
-    >>> growth_indicators(x, 'Authors')
-
-
-    >>> terms = ['author 3', 'author 4']
-    >>> growth_indicators(x, 'Authors', limit_to=terms)
-        Authors       AGR  ADY  PDLY  Before 2013  Between 2013-2014
-    0  author 3  0.666667  1.0  12.5            1                  2
-    1  author 4  0.000000  1.0  12.5            1                  2
-
-    >>> growth_indicators(x, 'Authors', exclude=terms)
-        Authors       AGR  ADY  PDLY  Before 2011  Between 2011-2014
-    0  author 1 -0.333333  0.5  6.25            1                  1
-    1  author 0 -0.333333  0.5  6.25            2                  1
-
-    """
-
-
 ###############################################################################
 ##
-##  DASHBOARD
+##  MODEL
 ##
 ###############################################################################
 
-COLUMNS = [
-    "Authors",
-    "Countries",
-    "Institutions",
-    "Author_Keywords",
-    "Index_Keywords",
-    "Abstract_words_CL",
-    "Abstract_words",
-    "Title_words_CL",
-    "Title_words",
-    "Affiliations",
-    "Author_Keywords_CL",
-    "Index_Keywords_CL",
-]
 
+class Model:
+    def __init__(self, data, limit_to, exclude, years_range):
+        ##
+        if years_range is not None:
+            initial_year, final_year = years_range
+            data = data[(data.Year >= initial_year) & (data.Year <= final_year)]
 
-class DASHapp(DASH):
-    def __init__(self, data, limit_to=None, exclude=None, years_range=None):
-        """Dashboard app"""
+        self.data = data
+        self.limit_to = limit_to
+        self.exclude = exclude
 
-        DASH.__init__(
-            self, data=data, limit_to=limit_to, exclude=exclude, years_range=years_range
-        )
-
-        self.app_title = "Growth Indicators"
-        self.menu_options = [
-            "Table",
-            "Average Growth Rate",
-            "Average Documents per Year",
-            "Percentage of Documents in Last Years",
-            "Num Documents",
-        ]
-
-        self.panel_widgets = [
-            dash.dropdown(
-                desc="Column:", options=[z for z in COLUMNS if z in self.data.columns],
-            ),
-            dash.dropdown(desc="Time window:", options=[2, 3, 4, 5],),
-            dash.separator(text="Visualization"),
-            dash.dropdown(
-                desc="Top by:",
-                options=[
-                    "Num Documents",
-                    "Times Cited",
-                    "Average Growth Rate",
-                    "Average Documents per Year",
-                    "Percentage of Documents in Last Years",
-                    "Number of Document Published",
-                    "Before",
-                    "Between",
-                ],
-            ),
-            dash.top_n(),
-            dash.dropdown(
-                desc="Sort by:",
-                options=[
-                    "Alphabetic",
-                    "Num Documents",
-                    "Times Cited",
-                    "Average Growth Rate",
-                    "Average Documents per Year",
-                    "Percentage of Documents in Last Years",
-                    "Before",
-                    "Between",
-                ],
-            ),
-            dash.ascending(),
-            dash.dropdown(desc="Plot:", options=["bar", "barh"],),
-            dash.cmap(),
-            dash.fig_width(),
-            dash.fig_height(),
-        ]
-        super().create_grid()
-
-    def interactive_output(self, **kwargs):
-
-        DASH.interactive_output(self, **kwargs)
-
-        if self.menu == "Table":
-            self.set_disabled("Plot:")
-            self.set_disabled("Colormap:")
-            self.set_disabled("Width:")
-            self.set_disabled("Height:")
-        else:
-            self.set_enabled("Plot:")
-            self.set_enabled("Colormap:")
-            self.set_enabled("Width:")
-            self.set_enabled("Height:")
-
-    def fit(self):
+    def apply(self):
         def average_growth_rate():
             #
             #         sum_{i=Y_start}^Y_end  Num_Documents[i] - Num_Documents[i-1]
@@ -338,11 +195,11 @@ class DASHapp(DASH):
         self.X_ = result
 
     def table(self):
-        self.fit()
+        self.apply()
         return self.X_
 
     def average_growth_rate(self):
-        self.fit()
+        self.apply()
         if self.plot == "bar":
             return plt.bar(
                 height=self.X_.AGR, cmap=self.cmap, figsize=(self.width, self.height)
@@ -353,7 +210,7 @@ class DASHapp(DASH):
             )
 
     def average_documents_per_year(self):
-        self.fit()
+        self.apply()
         if self.plot == "bar":
             return plt.bar(
                 height=self.X_.ADY, cmap=self.cmap, figsize=(self.width, self.height)
@@ -364,7 +221,7 @@ class DASHapp(DASH):
             )
 
     def percentage_of_documents_in_last_years(self):
-        self.fit()
+        self.apply()
         if self.plot == "bar":
             return plt.bar(
                 height=self.X_.PDLY, cmap=self.cmap, figsize=(self.width, self.height)
@@ -375,7 +232,7 @@ class DASHapp(DASH):
             )
 
     def num_documents(self):
-        self.fit()
+        self.apply()
         if self.plot == "bar":
             return plt.stacked_bar(
                 self.X_[[self.X_.columns[-2], self.X_.columns[-1]]],
@@ -388,6 +245,103 @@ class DASHapp(DASH):
                 cmap=self.cmap,
                 figsize=(self.width, self.height),
             )
+
+
+###############################################################################
+##
+##  DASHBOARD
+##
+###############################################################################
+
+COLUMNS = [
+    "Authors",
+    "Countries",
+    "Institutions",
+    "Author_Keywords",
+    "Index_Keywords",
+    "Abstract_words_CL",
+    "Abstract_words",
+    "Title_words_CL",
+    "Title_words",
+    "Affiliations",
+    "Author_Keywords_CL",
+    "Index_Keywords_CL",
+]
+
+
+class DASHapp(DASH, Model):
+    def __init__(self, data, limit_to=None, exclude=None, years_range=None):
+        """Dashboard app"""
+
+        Model.__init__(
+            self, data=data, limit_to=limit_to, exclude=exclude, years_range=years_range
+        )
+        DASH.__init__(self)
+
+        self.app_title = "Growth Indicators"
+        self.menu_options = [
+            "Table",
+            "Average Growth Rate",
+            "Average Documents per Year",
+            "Percentage of Documents in Last Years",
+            "Num Documents",
+        ]
+
+        self.panel_widgets = [
+            dash.dropdown(
+                desc="Column:", options=[z for z in COLUMNS if z in self.data.columns],
+            ),
+            dash.dropdown(desc="Time window:", options=[2, 3, 4, 5],),
+            dash.separator(text="Visualization"),
+            dash.dropdown(
+                desc="Top by:",
+                options=[
+                    "Num Documents",
+                    "Times Cited",
+                    "Average Growth Rate",
+                    "Average Documents per Year",
+                    "Percentage of Documents in Last Years",
+                    "Number of Document Published",
+                    "Before",
+                    "Between",
+                ],
+            ),
+            dash.top_n(),
+            dash.dropdown(
+                desc="Sort by:",
+                options=[
+                    "Alphabetic",
+                    "Num Documents",
+                    "Times Cited",
+                    "Average Growth Rate",
+                    "Average Documents per Year",
+                    "Percentage of Documents in Last Years",
+                    "Before",
+                    "Between",
+                ],
+            ),
+            dash.ascending(),
+            dash.dropdown(desc="Plot:", options=["bar", "barh"],),
+            dash.cmap(),
+            dash.fig_width(),
+            dash.fig_height(),
+        ]
+        super().create_grid()
+
+    def interactive_output(self, **kwargs):
+
+        DASH.interactive_output(self, **kwargs)
+
+        if self.menu == "Table":
+            self.set_disabled("Plot:")
+            self.set_disabled("Colormap:")
+            self.set_disabled("Width:")
+            self.set_disabled("Height:")
+        else:
+            self.set_enabled("Plot:")
+            self.set_enabled("Colormap:")
+            self.set_enabled("Width:")
+            self.set_enabled("Height:")
 
 
 ###############################################################################
