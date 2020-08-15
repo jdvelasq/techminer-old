087f0f8d0291ce9ab602f4d6e582ad8019c0bfe8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-08-15 21:44:48 +0000

    coverage

--- techminer/__init__.py
+++ techminer/__init__.py
@@ -1,15 +1,1 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
-# from . import load_data
-# from . import coverage
-# from . import descriptive_stats
-# from .chord_diagram import *
-# from .dataframe import *
-# from .keywords import *
-# from .maps import *
-# from .plot import *
-# from .text import *
-# from .thesaurus import *
-# from techminer.result import *
-# from techminer.common import *
-# from techminer.datasets import *
