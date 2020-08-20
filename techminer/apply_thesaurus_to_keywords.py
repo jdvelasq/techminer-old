09e4ee71b0ac4c6edb3ccea7b70ba1de2ebe29d8
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-08-20 12:40:35 +0000

    apply-thesaurus-to-keywords new module

--- techminer/apply_thesaurus_to_keywords.py
+++ techminer/apply_thesaurus_to_keywords.py
@@ -22,4 +22,30 @@ def apply_thesaurus_to_keywords(
     if include_index_keywords is True:
         df["Index_Keywords_CL"] = map_(df, "Index_Keywords", th.apply_as_dict)
 
+        #
+        # Create a new column 'Keywords'
+        #
+        df["Keywords_CL"] = pd.NA
+
+        cond = df.Author_Keywords_CL.map(
+            lambda w: not pd.isna(w)
+        ) & df.Index_Keywords_CL.map(lambda w: not pd.isna(w))
+        df.loc[cond, "Keywords_CL"] = (
+            df.Author_Keywords_CL[cond] + ";" + df.Index_Keywords[cond]
+        )
+
+        cond = df.Keywords_CL.map(pd.isna) & df.Author_Keywords_CL.map(
+            lambda w: not pd.isna(w)
+        )
+        df.loc[cond, "Keywords_CL"] = df.Author_Keywords_CL[cond]
+
+        cond = df.Keywords_CL.map(pd.isna) & df.Index_Keywords_CL.map(
+            lambda w: not pd.isna(w)
+        )
+        df.loc[cond, "Keywords_CL"] = df.Index_Keywords_CL[cond]
+
+        df.Keywords_CL = df.Keywords_CL.map(
+            lambda w: w if pd.isna(w) else ";".join(sorted(set(w.split(";"))))
+        )
+
     df.to_csv(output_file, index=False)
