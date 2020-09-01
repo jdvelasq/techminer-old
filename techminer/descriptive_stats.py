5794f45b6c6ef1b747d7fc748283818c846ba43f
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2020-09-01 01:48:22 +0000

    descriptive stats new table

--- techminer/descriptive_stats.py
+++ techminer/descriptive_stats.py
@@ -12,33 +12,6 @@ from techminer.core.params import EXCLUDE_COLS, MULTIVALUED_COLS
 
 
 def _extract_terms(x, column):
-    """Extracts unique terms in a column, exploding multvalued columns.
-
-    Args:
-        column (str): the column to explode.
-
-    Returns:
-        DataFrame.
-
-    Examples
-    ----------------------------------------------------------------------------------------------
-
-    >>> import pandas as pd
-    >>> x = pd.DataFrame({'Authors': ['xxx', 'xxx; zzz', 'yyy', 'xxx; yyy; zzz']})
-    >>> x
-             Authors
-    0            xxx
-    1       xxx; zzz
-    2            yyy
-    3  xxx; yyy; zzz
-
-    >>> _extract_terms(x, column='Authors')
-      Authors
-    0     xxx
-    1     yyy
-    2     zzz
-
-    """
     x = x.copy()
     x[column] = x[column].map(
         lambda w: w.split(";") if not pd.isna(w) and isinstance(w, str) else w
@@ -51,31 +24,6 @@ def _extract_terms(x, column):
 
 
 def _count_terms(x, column):
-    """Counts the number of different terms in a column.
-
-    Args:
-        x (pandas.DataFrame): Biblographic dataframe.
-        column (str): the column to explode.
-        sep (str): Character used as internal separator for the elements in the column.
-
-    Returns:
-        DataFrame.
-
-    Examples
-    ----------------------------------------------------------------------------------------------
-
-    >>> x = pd.DataFrame({'Authors': ['xxx', 'xxx; zzz', 'yyy', 'xxx; yyy; zzz']})
-    >>> x
-             Authors
-    0            xxx
-    1       xxx; zzz
-    2            yyy
-    3  xxx; yyy; zzz
-
-    >>> count_terms(x, column='Authors')
-    3
-
-    """
     return len(_extract_terms(x, column))
 
 
@@ -83,50 +31,176 @@ def descriptive_stats(input_file="techminer.csv"):
 
     x = pd.read_csv(input_file)
 
-    y = {}
-    y["Documents"] = str(len(x))
-    #
+    ##
+    ##  General information
+    ##
+    general = {}
+
+    general["Documents:"] = str(len(x))
+
+    if "Year" in x.columns:
+        general["Years:"] = str(min(x.Year)) + "-" + str(max(x.Year))
+        n = max(x.Year) - min(x.Year) + 1
+        Po = len(x.Year[x.Year == min(x.Year)])
+        Pn = len(x.Year[x.Year == max(x.Year)])
+        cagr = str(round(100 * (np.power(Pn / Po, 1 / n) - 1), 2)) + " %"
+        general["Compound annual growth rate:"] = cagr
+
+    if "Times_Cited" in x.columns:
+        general["Average citations per document:"] = "{:4.2f}".format(
+            x["Times_Cited"].mean()
+        )
+
+    if "Times_Cited" in x.columns and "Year" in x.columns:
+        general["Average citations per document per year:"] = "{:4.2f}".format(
+            x["Times_Cited"].sum() / (len(x) * (x.Year.max() - x.Year.min() + 1))
+        )
+
+    if "Global_References" in x.columns:
+        general["Total references:"] = round(_count_terms(x, "Global_References"))
+        general["Average global references per document:"] = round(
+            _count_terms(x, "Global_References") / len(x)
+        )
+
+    if "Source_Title" in x.columns:
+        general["Source titles:"] = round(_count_terms(x, "Source_title"))
+        general["Average documents per Source title:"] = round(
+            len(x) / _count_terms(x, "Source_title")
+        )
+        x.pop("Source_Title")
+
+    ##
+    ##  Document types
+    ##
+    document_types = {}
+    if "Document_Type" in x.columns:
+        z = x[["Document_Type"]].groupby("Document_Type").size()
+        for index, value in zip(z.index, z):
+            document_types[index + ":"] = value
+
+    ##
+    ##  Authors
+    ##
+    authors = {}
+
+    if "Authors" in x.columns:
+
+        authors["Authors:"] = _count_terms(x, "Authors")
+
+        m = x.Authors
+        m = m.dropna()
+        m = m.map(lambda w: w.split(";"), na_action="ignore")
+        m = m.explode()
+        authors["Author appearances:"] = len(m)
+
+        authors["Documents per author:"] = round(len(x) / _count_terms(x, "Authors"), 2)
+        authors["Authors per document:"] = round(_count_terms(x, "Authors") / len(x), 2)
+
+    if "Num_Authors" in x.columns:
+        authors["Single-authored documents:"] = len(x[x["Num_Authors"] == 1])
+        authors["Multi-authored documents:"] = len(x[x["Num_Authors"] > 1])
+        authors["Co-authors per document:"] = round(x["Num_Authors"].mean(), 2)
+        authors["Collaboration index:"] = round(
+            _count_terms(x[x.Num_Authors > 1], "Authors") / len(x[x.Num_Authors > 1]),
+            2,
+        )
+
+    if "Institutions" in x.columns:
+        authors["Institutions:"] = _count_terms(x, "Institutions")
+        x.pop("Institutions")
+
+    if "Countries" in x.columns:
+        authors["Countries:"] = _count_terms(x, "Countries")
+        x.pop("Countries")
+
+    ##
+    ##  Keywords
+    ##
+    keywords = {}
+
+    if "Author_Keywords" in x.columns:
+        keywords["Author Keywords (raw):"] = round(_count_terms(x, "Author_Keywords"))
+        x.pop("Author_Keywords")
+
+    if "Author_Keywords_CL" in x.columns:
+        keywords["Author Keywords (cleaned):"] = round(
+            _count_terms(x, "Author_Keywords_CL")
+        )
+        x.pop("Author_Keywords_CL")
+
+    if "Index_Keywords" in x.columns:
+        keywords["Author Keywords (raw):"] = round(_count_terms(x, "Index_Keywords"))
+        x.pop("Index_Keywords")
+
+    if "Index_Keywords_CL" in x.columns:
+        keywords["Index Keywords (cleaned):"] = round(
+            _count_terms(x, "Index_Keywords_CL")
+        )
+        x.pop("Index_Keywords_CL")
+
+    if "Title_words" in x.columns:
+        keywords["Title words (raw):"] = round(_count_terms(x, "Title_words"))
+        x.pop("Title_words")
+
+    if "Title_words_CL" in x.columns:
+        keywords["Title words (cleaned):"] = round(_count_terms(x, "Title_words_CL"))
+        x.pop("Title_words_CL")
+
+    if "Abstract_words" in x.columns:
+        keywords["Abstract words (raw)"] = round(_count_terms(x, "Abstract_words"))
+        x.pop("Abstract_words")
+
+    if "Abstract_words_CL" in x.columns:
+        keywords["Abstract words (cleaned)"] = round(
+            _count_terms(x, "Abstract_words_CL")
+        )
+        x.pop("Abstract_words_CL")
+
+    ##
+    ##  Report
+    ##
+
+    d = []
+    d += [key for key in general.keys()]
+    d += [key for key in document_types.keys()]
+    d += [key for key in authors.keys()]
+    d += [key for key in keywords.keys()]
+
+    v = []
+    v += [general[key] for key in general.keys()]
+    v += [document_types[key] for key in document_types.keys()]
+    v += [authors[key] for key in authors.keys()]
+    v += [keywords[key] for key in keywords.keys()]
+
+    ##
+    ##  Other columns in the dataset
+    ##
+
+    others = {}
     for column in sorted(x.columns):
 
-        if column in EXCLUDE_COLS:
+        if column in EXCLUDE_COLS or column + ":" in d:
             continue
 
-        if column != "Year":
-            y[column] = _count_terms(x, column)
+        others[column] = round(_count_terms(x, column))
 
-        if column == "Year":
-            y["Years"] = str(min(x.Year)) + "-" + str(max(x.Year))
-            n = max(x.Year) - min(x.Year) + 1
-            Po = len(x.Year[x.Year == min(x.Year)])
-            Pn = len(x.Year[x.Year == max(x.Year)])
-            cagr = str(round(100 * (np.power(Pn / Po, 1 / n) - 1), 2)) + " %"
-            y["Compound annual growth rate"] = cagr
+    if len(others):
+        d += [key for key in others.keys()]
+        v += [others[key] for key in others.keys()]
 
-        if column == "Times_Cited":
-            y["Average citations per document"] = "{:4.2f}".format(
-                x["Times_Cited"].mean()
-            )
-
-        if column == "Authors":
-            y["Documents per author"] = round(len(x) / _count_terms(x, "Authors"), 2)
-            y["Authors per document"] = round(_count_terms(x, "Authors") / len(x), 2)
-
-        if "Num_Authors" in x.columns:
-            y["Single-authored documents"] = len(x[x["Num_Authors"] == 1])
-            y["Multi-authored documents"] = len(x[x["Num_Authors"] > 1])
-            y["Co-authors per document"] = round(x["Num_Authors"].mean(), 2)
-
-        if "Source_Title" in x.columns:
-            y["Average documents per Source title"] = round(
-                len(x) / _count_terms(x, "Source_title")
-            )
-
-        if "Global_References" in x.columns:
-            y["Average global references per document"] = round(
-                _count_terms(x, "Global_References") / len(x)
-            )
-    #
-    d = [key for key in sorted(y.keys())]
-    v = [y[key] for key in sorted(y.keys())]
-    return pd.DataFrame(v, columns=["value"], index=d)
+    return pd.DataFrame(
+        v,
+        columns=["value"],
+        index=pd.MultiIndex.from_arrays(
+            [
+                ["GENERAL"] * len(general)
+                + ["DOCUMENT TYPES"] * len(document_types)
+                + ["AUTHORS"] * len(authors)
+                + ["KEYWORDS"] * len(keywords)
+                + ["OTHERS"] * len(others),
+                d,
+            ],
+            names=["Category", "Item"],
+        ),
+    )
 
